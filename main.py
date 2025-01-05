e880c60d0c5298fcf6fcf55c3b17a55a5601595c
hmokcode <mart1n.krocil@seznam.cz>
2025-01-05 12:29:36 +0100

    char count my attempt

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import string
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_path(book_path)
-    print(f"the number of words is: {get_amount(text)}")
     lowered_text = lower_case(text)
     split_string = split_char(lowered_text)
-
-    print(split_string)
+    count = count_char(split_string)
+    
+    print(count)
+    #print(split_string)
     #print(lowered_text)
+    #print(f"the number of words is: {get_amount(text)}")
     #print(text)
 
+def count_char(split_string):
+    alphabet = list(string.ascii_lowercase)
+    alphabet_count = []
+    for char in alphabet:
+        number = 0
+        for char_split in split_string:
+            if char == char_split:
+                number += 1
+        alphabet_count.append(f"{char}: {number}")
+    return alphabet_count
 
 def split_char(lowered_text):
     char_string = list(lowered_text)
