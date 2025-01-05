617b432c743d0515e8f08f8e61a2c43c0d4a5e8d
hmokcode <mart1n.krocil@seznam.cz>
2025-01-05 16:26:30 +0100

    converting dictionaries into list of them and sorting by highest number of chars, finishing L11

--- main.py
+++ main.py
@@ -3,14 +3,30 @@ def main():
     text = get_book_path(book_path)
     lowered_text = lower_case(text)
     split_string = split_char(lowered_text)
-    count = count_char(split_string)
-    
+    counted_chars = count_char(split_string)
+    converted = convert_dictionary(counted_chars)
+
     print(f"--- Begin report of {book_path} ---")
-    print(f"{get_amount(text)} words found in the document")
-    print(count)
+    print(f"{get_amount(text)} words found in the document \n")
+    converted.sort(reverse=True, key=sort_on)
+    for sort in converted:
+        print(f"The character '{sort["char"]}' was found {sort["amount"]} times")
+    print("--- End report ---")
+    #print(sort)
     #print(split_string)
     #print(lowered_text)
     #print(text)
+
+def sort_on(converted):
+    return converted["amount"]
+
+def convert_dictionary(counted_chars):
+    list_of_dict = []
+    for  chari, counti in counted_chars.items():
+        new_dict = {"char": chari, "amount": counti}
+        list_of_dict.append(new_dict)
+    return list_of_dict
+
 
 def count_char(split_string):
     alphabet_count = {}
