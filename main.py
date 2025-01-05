4fba8c6c45c6def043335699d79c7007c5913ff1
hmokcode <mart1n.krocil@seznam.cz>
2025-01-05 13:09:14 +0100

    more efficient count_char functiton- final

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import string
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_path(book_path)
@@ -13,14 +12,12 @@ def main():
     #print(text)
 
 def count_char(split_string):
-    alphabet = list(string.ascii_lowercase)
-    alphabet_count = []
-    for char in alphabet:
-        number = 0
-        for char_split in split_string:
-            if char == char_split:
-                number += 1
-        alphabet_count.append(f"{char}: {number}")
+    alphabet_count = {}
+    for char in split_string:
+        if char in alphabet_count:
+            alphabet_count[char] += 1
+        else:
+            alphabet_count[char] = 1
     return alphabet_count
 
 def split_char(lowered_text):
